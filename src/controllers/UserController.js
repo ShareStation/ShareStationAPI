847c97052b57f3794aa1ce7d77eda27b106284fb
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-12-18 21:47:13 -0200

    add socket and encryption on user password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,33 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const cryptPassword = (password, callback) => {
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, (error, salt) => {
+
+        if (error) return;
+
+        bcrypt.hash(
+            password,
+            salt,
+            (error, hash) => callback(error, hash)
+        );
+    });
+}
 
 module.exports = {
     async insertNewUser(req, res) {
         const userThatWillBeInserted = req.body;
+        const userPassword = userThatWillBeInserted.password;
 
-        const insertedUser = await User.create(userThatWillBeInserted);
+        cryptPassword(userPassword, async (error, hash) => {
+            userThatWillBeInserted.password = hash;
 
-        req.io.emit('newUserInserted', insertedUser);
-
-        return res.json(insertedUser);
+            const insertedUser = await User.create(userThatWillBeInserted);
+            
+            req.io.emit('newUserInserted', insertedUser);
+            
+            return res.json(insertedUser);
+        });
     }
 };
